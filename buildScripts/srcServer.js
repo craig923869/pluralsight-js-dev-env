80b2f6f9b7c949616817c337f66a205bbda54836
craig <craigduero@gmail.com>
2018-07-02 21:44:13 -0600

    wip 7/2

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,13 +1,20 @@
 var express = require('express');
-var path = require('path')
-var open = require('open')
+var path = require('path');
+var open = require('open');
 
 var port = 3000;
 var app = express();
 
+// const compiler = webpack(config);
+//
+// app.use(require('webpack-dev-middleware')(compiler, {
+//     noInfo: true,
+//     publicPath: config.output.publicPath
+// }))
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'))
-})
+});
 
 app.listen(port, function(err) {
     if(err) {
@@ -15,4 +22,4 @@ app.listen(port, function(err) {
     } else {
         open('http://localhost:' + port);
     }
-})
+});
